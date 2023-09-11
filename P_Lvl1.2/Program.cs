c6178c22bb476b6ecbf1ea05f391a435c0bf2110
Beytullah Ay <131589682+Bajbeyt@users.noreply.github.com>
2023-09-11 18:06:09 +0300

    Add files via upload

---
+++ P_Lvl1.2/Program.cs
@@ -0,0 +1,69 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        int totalScore = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Oyun başlıyor...");
+            char randomLetter = RandomLetter();
+            Console.WriteLine($"Rastgele harf: {randomLetter}");
+
+            List<string> titles = new List<string>
+            {
+                "İsim",
+                "Şehir",
+                "Hayvan",
+                "Bitki",
+                "Eşya",
+                "Ülke",
+                "Ünlü İsim"
+            };
+
+            foreach (string title in titles)
+            {
+                Console.Write($"Lütfen bir {title} girin : \n");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "q")
+                {
+                    break;
+                }
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Boş bırakılamaz.");
+                    continue;
+                }
+
+                if (input.ToLower()[0] == randomLetter)
+                {
+                    //Console.WriteLine("Doğru!");
+                    totalScore+=10;
+                }
+                else
+                {
+                    Console.WriteLine("Yanlış!");
+                }
+            }
+
+            Console.WriteLine($"Oyun sona erdi. \nToplam Puan: {totalScore}\n");
+            Console.Write("Başka bir oyun oynamak ister misiniz? \n (Evet: e / Hayır: herhangi bir tuş): ");
+            string playAgain = Console.ReadLine();
+
+            if (playAgain.ToLower() != "e")
+            {
+                break;
+            }
+        }
+    }
+
+    static char RandomLetter()
+    {
+        Random random = new Random();
+        char randomLetter = (char)('a' + random.Next(26));
+        return randomLetter;
+    }
+
+}
